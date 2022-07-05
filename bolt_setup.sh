5f6b1535ca9873f7dcfd424ac1e7510341bde5e7
Jiatao Gu <jiatao@apple.com>
2022-07-05 03:30:54 +0000

    install tcnn from source

--- bolt_setup.sh
+++ bolt_setup.sh
@@ -12,6 +12,10 @@ pip install gpustat torchinfo
 pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" git+https://github.com/NVIDIA/apex.git
 pip install -r requirements.txt
 
+pushd tinycudann/bindings/torch
+python setup.py install
+popd
+
 # just for committing code
 git config --global user.email "jiatao@apple.com"
 git config --global user.name  "Jiatao Gu"
