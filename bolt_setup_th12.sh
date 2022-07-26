a74f9029ef32a94897b7aeebb6db1e4499118ccb
Jiatao Gu <jiatao@apple.com>
2022-07-26 04:01:29 +0000

    do not install opencv again in this docker

--- bolt_setup_th12.sh
+++ bolt_setup_th12.sh
@@ -1,7 +1,7 @@
 python -m pip install --upgrade pip
 pip install gpustat torchinfo
 
-pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" git+https://github.com/NVIDIA/apex.git
+# pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" git+https://github.com/NVIDIA/apex.git
 pip install -r requirements.txt
 pip install tinycudann/bindings/torch
 
